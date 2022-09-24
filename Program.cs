3f497227965ce25d72f8818b74967c3650ca6b0a
HP <HP@NAYDU>
2022-09-24 09:22:17 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using RamonPC02.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//Get ConnectionString
+var connectionString = builder.Configuration.GetConnectionString("DevConnection");
+//Add DbContext
+builder
+    .Services
+    .AddDbContext<SalesUESANContext>
+    (options => options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
